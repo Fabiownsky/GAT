069ef85018dc57843b819e6cfc5598b78b29a84f
Fabio <gporcelli64@gmail.com>
2024-06-17 14:37:50 +0200

    Added test files + assets folder

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,13 @@
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            System.out.println("Hello, World!");
+            System.out.println("Press Enter to exit...");
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 }
